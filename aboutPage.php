5e24dea0990835c425a24124b32091fd7d55ecce
Oshani Weerakoon <osw0294@gmail.com>
2018-01-20 16:35:17 +0530

    about

---
+++ aboutPage.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About Us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="commonPage.css">
+
+</head>
+<body>
+<div id="container">
+	<div id="header">
+		<div id="signin">
+			<button class="button" id="signinbtn">Already a member?</button>
+			<button class="button">Sign Up</button>
+		</div>
+		<div id="logo">
+			<img src="./img/logo.jpg">
+		</div>
+		<div>
+			<p id="stext1"> Welcome to QuickBuy.lk !</p>
+		</div>
+		<div id="top_info">
+				<form id="searchform" action="searchResults.php" method="post">
+				
+				<input type="text" name="searchText" id="stext2" placeholder="Search Here" required>
+				<input type="submit" id="sbutton" name="search" value="Search">
+			</form>
+		</div>
+		<div id="navbar">
+			<ol id="menu">
+				<li><a href="./categoryPage.php">Categories</a>
+  				</li>
+				<li><a href="./contactPage.php">Help & Contact</a></li>
+				<li><a href="./aboutPage.php">About Us</a></li>
+				<li><a href="./myAccount.php">My Account</a></li>
+			</ol>
+		</div>
+	</div>
+</div>
+
+
+<div id="content_area" >
+	<div align="center">
+		<p style="margin: 100px;"><img src="./img/logo.jpg" width="20%" height="20%"><br>
+		</p>
+	</div>
+
+	<div align="center">
+		<p style="font-size: 20px; margin: 0px 25% 0px 25%">
+			QuickBuy.lk is a online platform which acts as a online market place for buying and selling of mobile phones which was founded in 2011. Founder of QuickBuy.lk, mr. Rahal Gunasekara, a proud graduate of University of Colombo school of Computing, guides the entire organization to the success! Since May 2017, QuickBuy.lk has been partnered with ikman.lk as their official mobile advertisement handeling partner for next five years.
+			<br><br>
+			With a staff more than 50 workers, QuickBuy.lk is proud to offer you a trusted service with to-be-introduced PayPal secured online payment system.  
+		</p>
+	</div>
+</div>
+
+</body>
+</html>
